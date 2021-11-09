9bcec6abf65690fa7b437e8b8345cbf1c0aea8c8
GabrielChan <neoco@techie.com>
2021-11-09 11:11:58 -0500

    Modified queries.sql

--- linux_sql/Scripts/queries.sql
+++ linux_sql/Scripts/queries.sql
@@ -3,5 +3,14 @@ SELECT cpu_number, id, total_mem OVER(PARTITION BY cpu_number)
 FROM host_info
 ORDER BY total_mem DESC
 
+/*
+ CREATE FUNCTION function_name (@input input_data_type)
+ RETURNS output_data_type
+ AS
+ BEGIN
+    * Find 5 minute interval here
+ END
+ * Need to run this function multiple times to get all the 5 minute intervals
+ */
+
 -- Find average memory usage of each host
-SELECT host_id, host_name, timestamp, (total_mem - memory_free)
