c29070c9acc0ff8df504f9a720ef25470ab51742
GabrielChan <neoco@techie.com>
2022-06-27 14:12:49 -0400

    Solved Valid Parentheses and Valid Palindrome coding challenges

--- practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidPalindrome.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidPalindrome.java
@@ -1,2 +1,42 @@
-package ca.jrvs.practice.codingChallenge;public class ValidPalindrome {
+package ca.jrvs.practice.codingChallenge;
+
+public class ValidPalindrome {
+    /*
+    - Convert all uppercase letters to lowercase letters
+    - Remove all non-alphanumeric characters
+    - If the length of the string is 0 or 1, return true immediately
+    - Otherwise, check if the first and last characters are equal and iterate towards the middle of the string
+        - If, at any point, the two characters are not equal, return false immediately
+        - Once the middle of the string has been reached, return true
+    */
+    public boolean twoPointersSolution(String string) {
+        String s = string.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        if (s.length() <= 1) {
+            return true;
+        }
+
+        int i = 0;
+        int j = s.length() - 1;
+        do {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false;
+            }
+            i++;
+            j--;
+        } while (j - i > 0);
+        return true;
+    }
+
+    public boolean recursiveSolution(String string) {
+        String s = string.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        if (s.length() <= 1) {
+            return true;
+        }
+        else if (s.charAt(0) != s.charAt(s.length() - 1)) {
+            return false;
+        }
+        else {
+            return recursiveSolution(s.substring(1, s.length() - 1));
+        }
+    }
 }
--- practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidParentheses.java
+++ practice/src/main/java/ca/jrvs/practice/codingChallenge/ValidParentheses.java
@@ -6,19 +6,17 @@ import java.util.Stack;
 public class ValidParentheses {
     public boolean validParentheses(String s) {
         /*
-        Iterate through the string checking for opening brackets. When there is an opening bracket, add it to the
+        -Iterate through the string checking for opening brackets. When there is an opening bracket, add it to the
         'openingBrackets' stack.
-
-        While there is an opening bracket in the 'openingBrackets' stack:
+        - While there is an opening bracket in the 'openingBrackets' stack:
             - If another opening bracket appears, add it to the 'openingBrackets' stack and replace the current opening
             bracket with it.
             - If a closing bracket appears that doesn't match the current opening bracket, return 'false' immediately.
             - If a closing bracket appears that matches the current opening bracket, remove the current opening
             bracket from the 'openingBrackets' stack. If there are still brackets in the 'openingBrackets' stack, the
             next bracket in the stack replaces the current opening bracket.
-
-        If a closing bracket appears while there are no opening brackets, return 'false' immediately.
-        If the 'openingBrackets' stack is not empty when we reach the end of the string, return 'false'.
+        - If a closing bracket appears while there are no opening brackets, return 'false' immediately.
+        - If the 'openingBrackets' stack is not empty when we reach the end of the string, return 'false'.
         */
         Stack<Character> openingBrackets = new Stack<>();
         Character currentOpenBracket = null;
